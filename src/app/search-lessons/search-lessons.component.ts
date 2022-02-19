081aea10adc29cfdf5607087e82506cc76a4de01
fahmydiab <king.diab.007@gmail.com>
2022-02-19 23:44:59 +0200

    - add search lessons method

--- src/app/search-lessons/search-lessons.component.ts
+++ src/app/search-lessons/search-lessons.component.ts
@@ -1,20 +1,7 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  startWith,
-  tap,
-  delay,
-  map,
-  concatMap,
-  switchMap,
-  withLatestFrom,
-  concatAll, shareReplay
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import {CoursesService} from '../services/coursers.service';
 
 
 @Component({
@@ -24,7 +11,10 @@ import {Lesson} from '../model/lesson';
 })
 export class SearchLessonsComponent implements OnInit {
 
-  constructor() {
+  searchResults$: Observable<Lesson[]>;
+  activeLesson: Lesson;
+
+  constructor(private coursesService: CoursesService) {
 
 
   }
@@ -34,6 +24,17 @@ export class SearchLessonsComponent implements OnInit {
 
   }
 
+  onSearch(search: string) {
+    this.searchResults$ = this.coursesService.searchLessons(search);
+  }
+
+  openLesson(lesson: Lesson) {
+    this.activeLesson = lesson;
+  }
+
+  onBackToSearch() {
+    this.activeLesson = null;
+  }
 }
 
 
