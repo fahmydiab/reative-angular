5b9d54ac923b43f5be69e0f6884a4b08b8b8f64b
fahmydiab <king.diab.007@gmail.com>
2022-02-19 23:12:29 +0200

    - implement Authentication state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthStore} from './services/auth.store';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements  OnInit {
 
-    constructor() {
+    constructor(public auth: AuthStore) {
 
     }
 
@@ -17,7 +18,7 @@ export class AppComponent implements  OnInit {
     }
 
   logout() {
-
+    this.auth.logout();
   }
 
 }
