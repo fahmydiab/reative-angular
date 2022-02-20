4594826624443e2ed50ed0432d86387bbc3248d6
fahmydiab <king.diab.007@gmail.com>
2022-02-20 14:42:28 +0200

    - add course lessons component

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,45 +1,54 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  startWith,
-  tap,
-  delay,
-  map,
-  concatMap,
-  switchMap,
-  withLatestFrom,
-  concatAll, shareReplay, catchError
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import {combineLatest, Observable, scheduled} from 'rxjs';
+import {CoursesService} from '../services/coursers.service';
+import {map, startWith, tap} from 'rxjs/operators';
 
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 @Component({
   selector: 'course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseComponent implements OnInit {
 
-  course: Course;
+  data$: Observable<CourseData>;
 
-  lessons: Lesson[];
-
-  constructor(private route: ActivatedRoute) {
-
-
+  constructor(private route: ActivatedRoute,
+              private coursesService: CoursesService) {
   }
 
   ngOnInit() {
+    const courseId = parseInt(this.route.snapshot.paramMap.get('courseId'),  null);
 
+     const course$ = this.coursesService.loadCourseById(courseId)
+       .pipe(
+         startWith(null)
+       );
 
+    const lessons$ = this.coursesService.loadAllCourseLessons(courseId)
+      .pipe(
+        startWith(null)
+      );
 
+    this.data$ = combineLatest([course$, lessons$])
+      .pipe(
+        map(([course, lessons]) => {
+          return {
+            course,
+            lessons
+          };
+        }),
+        tap(console.log)
+      );
   }
-
-
 }
 
 
